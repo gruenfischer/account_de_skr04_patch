095f786bd4828041441e65a20d0d657a87f5991b
Jakob Fischer <jakob.fischer@gruenfischer.de>
2025-07-18 21:41:12 +0200

    fix: aktualisiere setup.py zur Verbesserung der Abhängigkeiten und Moduleinstellungen

--- setup.py
+++ setup.py
@@ -1,104 +1,107 @@
-#!/usr/bin/env python3
-import io
-import os
+# -*- coding: utf-8 -*-
+# This file is part of the account-de-skr04-module for Tryton from m-ds.de.
+# The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from setuptools import setup
+from codecs import open
+from os import path
 import re
 from configparser import ConfigParser
-from setuptools import find_packages, setup
 
-MODULE2PREFIX = {
-    'account_de_skr04': 'gf_mds',
-}
+here = path.abspath(path.dirname(__file__))
+MODULE = 'account_de_skr04'
+PREFIX = 'gf-mds'  # Wichtig: Bindestriche für pip-kompatiblen Namen
 
-def read(fname):
-    content = io.open(
-        os.path.join(os.path.dirname(__file__), fname),
-        'r', encoding='utf-8').read()
-    content = re.sub(
-        r'(?m)^\.\. toctree::\r?\n((^$|^\s.*$)\r?\n)*', '', content)
-    return content
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-def get_require_version(name):
-    require = '%s >= %s.%s, < %s.%s'
-    require %= (name, major_version, minor_version,
-                major_version, minor_version + 1)
-    return require
-
+# tryton.cfg einlesen
 config = ConfigParser()
-config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
+config.read_file(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
 
-version = info.get('version', '0.0.1')
-major_version, minor_version, _ = version.split('.', 2)
-major_version = int(major_version)
-minor_version = int(minor_version)
-name = 'gf_account_de_skr04_patch'
+# Get module-versions
+modversion = {}
+with open(path.join(here, 'versiondep.txt'), encoding='utf-8') as f:
+    l1 = f.readlines()
+    for i in l1:
+        l2 = i.strip().split(';')
+        if len(l2) < 4:
+            continue
+        modversion[l2[0]] = {'min': l2[1], 'max': l2[2], 'prefix': l2[3]}
 
-# ⚠️ fix: replace PyPI dependency with GitHub direct link
-requires = [
-    "gf-mds-account-de-skr04 @ git+https://github.com/gruenfischer/gf_mds_account_de_skr04.git@v7.4.3",
-    get_require_version('trytond'),
-]
+# tryton-version
+major_version = 7
+minor_version = 4
 
+requires = []
 for dep in info.get('depends', []):
-    if not re.match(r'(ir|res)(\W|$)', dep):
-        prefix = MODULE2PREFIX.get(dep, 'trytond')
-        requires.append(get_require_version('%s_%s' % (prefix, dep)))
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        if dep in modversion.keys():
+            prefix = 'mds'
+            if len(modversion[dep]['prefix']) > 0:
+                prefix = modversion[dep]['prefix']
 
-tests_require = []
+            if len(modversion[dep]['max']) > 0:
+                requires.append('%s_%s >= %s, <= %s' % (
+                    prefix, dep, modversion[dep]['min'],
+                    modversion[dep]['max']))
+            else:
+                requires.append('%s_%s >= %s' % (
+                    prefix, dep, modversion[dep]['min']))
+        else:
+            requires.append('%s_%s >= %s.%s, < %s.%s' % (
+                'trytond', dep, major_version, minor_version,
+                major_version, minor_version + 1))
+requires.append('trytond >= %s.%s, < %s.%s' % (
+    major_version, minor_version, major_version, minor_version + 1))
 
 setup(
-    name=name,
-    version=version,
-    description='Extends the account_de_skr04 module (maintained by Martin Data Service GmbH Berlin) with taxes and tax codes for easy tax reporting.',
-    long_description=read('README.rst'),
-    author='Grünfischer Consulting',
-    author_email='jakob.fischer@gruenfischer.de',
-    url='https://www.gruenfischer.de',
-    keywords='accounting tax skr04 Tryton',
-    package_dir={'trytond.modules.account_de_skr04_patch': '.'},
-    packages=(
-        ['trytond.modules.account_de_skr04_patch']
-        + ['trytond.modules.account_de_skr04_patch.%s' % p
-            for p in find_packages()]
-    ),
-    package_data={
-        'trytond.modules.account_de_skr04_patch': (
-            info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
-               'icons/*.svg', 'tests/*.rst', 'tests/*.json']
-        ),
-    },
+    name='%s-%s' % (PREFIX, MODULE),  # Wichtig: Bindestrich statt Unterstrich
+    version=info.get('version', '0.0.1'),
+    description='Tryton module with German chart of accounts SKR04',
+    long_description=long_description,
+    url='https://www.m-ds.de/',
+    author='m-ds GmbH',
+    author_email='service@m-ds.de',
+    license='GPL-3',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: Information Technology',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Office/Business :: Financial :: Accounting',
         'Natural Language :: German',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Office/Business',
     ],
-    license='GPL-3',
-    python_requires='>=3.9',
-    install_requires=requires,
-    extras_require={
-        'test': tests_require,
+
+    keywords='tryton account chart german SKR04',
+    package_dir={'trytond.modules.%s' % MODULE: 'trytond_account_de_skr04'},
+    packages=['trytond.modules.%s' % MODULE],
+    package_data={
+        'trytond.modules.%s' % MODULE: (
+            info.get('xml', []) +
+            ['tryton.cfg', 'LICENSE', 'README.rst']
+        ),
     },
+
+    install_requires=requires,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_de_skr04_patch = trytond.modules.account_de_skr04_patch
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
 )
